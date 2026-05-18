c7451284082379bd1c8cccec479ae12af981e6b4
luke <luke123@foxmail.com>
2026-05-18 22:24:56 +0800

    chore: 初始化项目基础结构和多平台配置
    1. 创建flutter admin、client客户端和core库项目基础文件
    2. 搭建后端user、chat、memory服务和gateway网关基础框架
    3. 完善各平台编译配置、资源文件和构建脚本

---
+++ app/vex_owl_admin/android/app/src/main/kotlin/com/example/vex_owl_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vex_owl_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app/vex_owl_client/android/app/src/main/kotlin/com/example/vex_owl_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vex_owl_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
